d0e9b780fc7226aa30efa57b88b283e9a713260b
telbohtimy <elbohtim@ualberta.ca>
2015-01-19 14:26:39 -0800

    Licence

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import SocketServer
 # coding: utf-8
 
-# Copyright 2013 Abram Hindle, Eddie Antonio Santos
+# Copyright 2013 Abram Hindle, Eddie Antonio Santos,Tarek El Bohtimy
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
